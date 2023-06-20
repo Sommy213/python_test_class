e424ef88be1032a16b41d42ffeaddce6b39639f2
sommy <chisomas32@gmail.com>
2023-06-20 09:07:59 +0100

    update: added new file

---
+++ class/class.py
@@ -0,0 +1,39 @@
+# # full_name = 'alex'
+# # names = ['chisom','precious']
+# # print(full_name[::-1])
+# # # print((name)[-len])
+# # s =[i for i in range (10)]
+# # print(s)
+# # def  convert_to_f(vaule):
+# #     result = (vaule*(9/5))+32
+# #     return result
+# # c_temp =[1,2,3,4,5]
+# # f_temp =[1convert_to_f(f(temp) for in c_temp)]
+# # for temp_c
+# # for b in w:
+# #     for c in d:
+# #         if w==try
+# a=[1,2,3,4,5,6,7,8,9]
+# b=[2,4,6,8,10,12,14,16,18,118,20]
+
+# c = [(x%y) if x%y  else 1 for x in b for y in a ]
+# c = [(x%y)  for x in b for y in a if x%y  ]
+# #  if u have an else start with the first
+# print(c)
+
+def multiply_even_number():
+    
+    nums=[n for n in range (100) if multiply_even_number ([int (j)for j in str(n*2)]) % 2 == 0]
+    
+print[nums]       
+
+# thresehold =10
+# x =20
+# flag='over'if x>thresehold else 'under'  
+# print(flag)  
+# age_limit=18
+# user_age = 17
+# age_limit ='you are not old enough'if user_age  < age_limit else 'you are old enough'
+# print(age_limit)
+
+        
